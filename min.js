3c856417bab4b17dabd1bdea13330604d53c3b8f
gzeus0911 <143071366+gzeus0911@users.noreply.github.com>
2025-09-05 12:41:31 +0700

    Update min.js

--- min.js
+++ min.js
@@ -2,9 +2,6 @@ window.addEventListener("load", function() {
     const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     console.log("User Timezone:", userTimeZone);
     const targetTimeZones = [
-      "Asia/Jakarta",    // Indonesia - Jakarta
-      "Asia/Makassar",   // Indonesia - Makassar
-      "Asia/Jayapura",   // Indonesia - Jayapura
       "Asia/Phnom_Penh",  // Campuchia - Phnom Penh
       "Asia/Ho_Chi_Minh",
       "Asia/Saigon"
